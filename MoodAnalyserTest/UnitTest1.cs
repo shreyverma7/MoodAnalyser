66bed25d30120332891c2b19e2624538681aff88
Shrey Kumar Verma <shreykumar.v@tvsnext.com>
2023-08-04 10:16:49 +0530

    [Shrey] Add : Analyser Mood Utest

--- MoodAnalyserTest/UnitTest1.cs
+++ MoodAnalyserTest/UnitTest1.cs
@@ -1,16 +1,29 @@
+using MoodAnalyserProblem;
+
 namespace MoodAnalyserTest
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        MoodAnalyser analyser = new MoodAnalyser();
+        [Test]
+        public void GivenSadMessage_WhenAnalyse_ShouldReturnSad()
         {
+            string message = "I am in Sad Mood";
+            string acutual = analyser.AnalyseMode(message);
+            Assert.AreEqual(acutual, "SAD");
+        }
+        [Test]
+        public void GivenAnyMessage_WhenAnalyse_ShouldReturnHappy()
+        {
+            string message = "I am in any Mood";
+            string acutual = analyser.AnalyseMode(message);
+            Assert.AreEqual(acutual, "HAPPY");
         }
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
+
+
+
+
+
     }
 }
